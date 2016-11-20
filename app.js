5bdd5fee5f41611c2a3863d57c8f051366851712
Shay Zaltsman <Shay@native.local>
2016-11-20 19:57:07 +0200

    from new computer

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-console.log("fileserver - hi123");
-process.exit();
+http = require("http");
+req=require("querystring");
+var server=http.createServer();
+server.on("request",function(req,res){
+    console.log("action");
+    var sData="";
+    req.on("error",function(err){
+        console.log (err.stack);
+    }).on("data",function(chunk){
+        sData+=chunk;
+    }).on("end",function(){
+        res.end(sData);
+    })
+}).listen(8050);
