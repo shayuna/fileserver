a2c5b174f0c60b91dd45ce27b727c293f47a133d
shayuna <shaiz2206@gmail.com>
2016-11-20 22:19:00 +0200

    using express

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-http = require("http");
-req=require("querystring");
-var server=http.createServer();
-server.on("request",function(req,res){
-    console.log("action");
-    var sData="";
-    req.on("error",function(err){
-        console.log (err.stack);
-    }).on("data",function(chunk){
-        sData+=chunk;
-    }).on("end",function(){
-        res.end(sData);
-    })
-}).listen(8050);
+var express = require('express');    //Express Web Server 
+var busboy = require('connect-busboy'); //middleware for form/file upload
+var path = require('path');     //used for file path
+var fs = require('fs-extra');       //File System - for file manipulation
+
+var app = express();
+app.use(busboy());
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* ========================================================== 
+Create a Route (/upload) to handle the Form submission 
+(handle POST requests to /upload)
+Express v4  Route definition
+============================================================ */
+app.route('/upload')
+    .post(function (req, res, next) {
+
+        var fstream;
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename) {
+            console.log("Uploading: " + filename);
+
+            //Path where image will be uploaded
+            fstream = fs.createWriteStream(__dirname + '/test_files/' + filename);
+            file.pipe(fstream);
+            fstream.on('close', function () {    
+                console.log("Upload Finished of " + filename);              
+                res.redirect('back');           //where to go next
+            });
+        });
+    });
+
+var server = app.listen(3030, function() {
+    console.log('Listening on port %d', server.address().port);
+});
