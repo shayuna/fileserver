0341cbceb8468d7a65769273ebdcd9f01fb48559
shayuna <shaiz2206@gmail.com>
2016-11-21 00:07:11 +0200

    first working version

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ app.route('/upload')
         req.busboy.on('file', function (fieldname, file, filename) {
             console.log("Uploading: " + filename);
 
-            var sFileNm=filename.replace(/[.].+/ig,"");
-            var sFolderNm=getFolderNm(sFileNm);
+            var sFolderNm=getFolderNm(filename);
            dirExists(__dirname + "\\"+sFolderNm+"\\");
             fstream = fs.createWriteStream(__dirname + "\\"+sFolderNm+"\\" + filename);
             file.pipe(fstream);
@@ -39,13 +38,18 @@ app.route("/download").get(function (req, res, next) {
 
     var sFileNm=req.query.flNm;
     var sFolderNm=__dirname + "\\"+getFolderNm(sFileNm);
-    var sPath=sFolderNm+"\\"+sFileNm+".txt";
-    var readStream = fs.createReadStream(sPath);
-    // We replaced all the event handlers with a simple call to readStream.pipe()
-    readStream.pipe(res);
-    res.on("close",function(){
-        console.log("finished downloading - "+sFileNm);
-    })
+    var sPath=sFolderNm+"\\"+sFileNm;
+    if (fs.existsSync(sPath)){
+        var readStream = fs.createReadStream(sPath);
+        // We replaced all the event handlers with a simple call to readStream.pipe()
+        readStream.pipe(res);
+        console.log("finished downloading - "+sPath);
+    }
+    else{
+        res.statusCode=404;
+        res.end("didn't find - "+sPath);
+        console.log ("didn't find - "+sPath);
+    }
 })
 
 
@@ -54,7 +58,7 @@ function dirExists(sPath){
 }
 
 function getFolderNm(sFileNm){
-    var sFolderNm="remainder";
+    var sFolderNm="remainder",sFileNm=/^\d+/ig.exec(sFileNm)[0];
     if (!isNaN(sFileNm)){
         sFolderNm=Math.floor(parseFloat(sFileNm)/1000+1);
     }
